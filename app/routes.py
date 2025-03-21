62ce138f81e36fc663f045aa5739cba5135100f6
Arturs Ziemelis <aziemelis@proton.me>
2025-03-21 20:26:40 +0200

    update prometheus and grafana

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 from app import boxscore
 from . import app  # Import Flask application
+from prometheus_flask_exporter import PrometheusMetrics
+
+metrics = PrometheusMetrics(app)
+@app.route('/metrics')
+def metrics():
+    return metrics.handler()
 
 
 @app.route("/")
